0f6ed4c675fbc07dbb94ca231cb4b545652b2aaa
YaninaGolubenko <102427471+YaninaGolubenko@users.noreply.github.com>
2023-07-10 13:52:17 +0300

    Update ProgressBar.js

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -1,9 +1,9 @@
 const ProgressBar = ({progress}) => {
   const colors = [
     'rgb(255, 214, 161)',
+    'rgb(201, 143, 255)',
     'rgb(255, 175, 163)',
-    'rgb(108, 115, 148)',
-    'rgb(141, 181, 145)'
+    'rgb(182, 223, 186)'
   ]
 
   const randomColor = colors[Math.floor(Math.random() * colors.length)]
